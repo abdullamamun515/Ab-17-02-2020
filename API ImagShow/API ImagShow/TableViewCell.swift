4fbb8ba6bd9bcee6990a079385b3461312cdffa9
Apple MacBook Pro <Apple@Apples-MacBook-Pro-2.local>
2020-02-18 09:49:16 +0600

    Collection View

---
+++ API ImagShow/API ImagShow/TableViewCell.swift
@@ -0,0 +1,25 @@
+//
+//  TableViewCell.swift
+//  API ImagShow
+//
+//  Created by Apple MacBook Pro on 2/17/20.
+//  Copyright © 2020 Apple MacBook Pro. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageViewCell: UIImageView!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
